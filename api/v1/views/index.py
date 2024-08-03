9a29496539ce348ba0ab7b7b12bd0a534bbeb326
basma ebraheim <basmaebraheim@gmail.com>
2024-08-03 13:04:24 +0300

    start web api

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """return status"""
+    return jsonify({"status": "OK"})
+
+
+if __name__ == "__main__":
+    pass
