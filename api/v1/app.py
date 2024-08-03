aa3e1ebd2eb3d928fe434992a2c1f7c76da334d4
basma ebraheim <basmaebraheim@gmail.com>
2024-08-03 23:23:22 +0300

    update states

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,12 @@ import os
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -23,4 +25,4 @@ def page_not_found(error):
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')), threaded=True)
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 """start flask api"""
-
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
 
-classes = {"Amenity": "Amenity", "BaseModel": "BaseModel", "City": "City",
-           "Place": "Place", "Review": "Review", "State": "State", "User": "User"}
+classes = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route('/status', strict_slashes=False)
-def status():
+def get_status():
     """return status"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
-def stats():
+def get_stats_counts():
     """retrieves the number of each objects by type"""
     stats_counts = {}
     for key, value in classes.items():
