e1ae339d5e72dfe10637dc50b75601da29a4d6eb
basma ebraheim <basmaebraheim@gmail.com>
2024-08-03 13:35:40 +0300

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -18,6 +19,7 @@ def teardown_db(exception):
 @app.errorhandler(404)
 def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
